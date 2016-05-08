ac26bb376a2d7bd902dd90f6e15a01d80b9c7c3d
Jonas <jonas.kemper.biz@gmail.com>
2016-05-08 16:46:52 +0200

    initial commit

---
+++ js/controllers.js
@@ -0,0 +1,14 @@
+angular.module('app.controllers', [])
+  
+.controller('carTabDefaultPageCtrl', function($scope) {
+
+})
+   
+.controller('locationTabDefaultPageCtrl', function($scope) {
+
+})
+   
+.controller('batteryTabDefaultPageCtrl', function($scope) {
+
+})
+    
