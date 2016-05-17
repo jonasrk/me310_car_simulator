0f693dda3054e752bb1f774442445cc3cce7280f
Jonas <jonas.kemper.biz@gmail.com>
2016-05-17 16:00:08 +0200

    New Interface for Weather data

--- js/controllers.js
+++ js/controllers.js
@@ -1,31 +1,55 @@
 angular.module('app.controllers', [])
-  
-.controller('carTabDefaultPageCtrl', function($scope, socket) {
-    
 
-})
-   
-.controller('locationTabDefaultPageCtrl', function($scope, socket) {
+    .controller('carTabDefaultPageCtrl', function($scope, socket) {
 
-})
-   
-.controller('batteryTabDefaultPageCtrl', function($scope, socket) {
-    
-    $scope.tval = 5;
-    
-    $scope.updateBattery = function (tval) {
-        console.log(("New Battery level: " + tval));
 
-        socket.emit('updateBattery', tval);
-    }
+    })
 
-    $scope.oval = 5;
+    .controller('locationTabDefaultPageCtrl', function($scope, socket) {
+        
+        $scope.choice = null;
+        
+        $scope.updateWeather = function (choice) {
+            console.log(("New Weather: " + choice));
 
-    $scope.updateOil = function (oval) {
-        console.log(("New Oil level: " + oval));
+            socket.emit('updateWeather', choice);
+        };
 
-        socket.emit('updateOil', oval);
-    }
+        $scope.time_val = 6;
 
-})
+        $scope.updateTime = function (time_val) {
+            console.log(("New Time: " + time_val));
+
+            socket.emit('updateTime', time_val);
+        };
+
+        $scope.temp_val = 72;
+
+        $scope.updateTemperature = function (temp_val) {
+            console.log(("New Temperature: " + temp_val));
+
+            socket.emit('updateTemperature', temp_val);
+        };
+
+    })
+
+    .controller('batteryTabDefaultPageCtrl', function($scope, socket) {
+
+        $scope.tval = 5;
+
+        $scope.updateBattery = function (tval) {
+            console.log(("New Battery level: " + tval));
+
+            socket.emit('updateBattery', tval);
+        };
+
+        $scope.oval = 5;
+
+        $scope.updateOil = function (oval) {
+            console.log(("New Oil level: " + oval));
+
+            socket.emit('updateOil', oval);
+        };
+
+    })
     
